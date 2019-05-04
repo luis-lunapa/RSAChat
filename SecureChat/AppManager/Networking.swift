37bfd4b114a5a142b37b03df49bd393a11e05988
luisLuna24 <luis.lunapa@outlook.com>
2019-05-04 14:25:08 -0500

    Se agrego clase de networking

---
+++ SecureChat/AppManager/Networking.swift
@@ -0,0 +1,9 @@
+//
+//  Networking.swift
+//  SecureChat
+//
+//  Created by Luis Luna on 5/4/19.
+//  Copyright © 2019 DeepTech. All rights reserved.
+//
+
+import Foundation
---
+++ SecureChat/SwiftyJSON.swift
@@ -0,0 +1,9 @@
+//
+//  SwiftyJSON.swift
+//  SecureChat
+//
+//  Created by Luis Luna on 5/4/19.
+//  Copyright © 2019 DeepTech. All rights reserved.
+//
+
+import Foundation
